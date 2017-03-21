6d15710d844da6409b96a7bd754d620f2eb154cb
mimikun <mimikun@192.168.1.101>
2017-03-21 07:18:59 +0100

    Added author tag

--- src/main/java/org/formacio/mvc/ControllerGestioAgenda.java
+++ src/main/java/org/formacio/mvc/ControllerGestioAgenda.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * @author mimikun
+ */
 @Controller
 public class ControllerGestioAgenda {
 
